9a6348a13f846364660123e42dfaa418d2ea1962
Milli-saved <million12.tenkir@gmail.com>
2022-10-31 14:35:57 +0300

    working on topbar

--- src/views/global/Topbar.jsx
+++ src/views/global/Topbar.jsx
@@ -1,7 +1,38 @@
-
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import { InputBase } from "@mui/material";
+import {
+  LightModeOutlined,
+  DarkModeOutlined,
+  NotificationsOutlined,
+  SettingsOutlined,
+  PersonOutline,
+  SearchOutlined,
+} from "@mui/icons-material";
 
 const Topbar = () => {
-  return <div>Topbar</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      {/** the search bar */}
+      <Box
+        display="flex"
+        backgroundColor={colors.primary[400]}
+        borderRadius="3px"
+      >
+        <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+        <IconButton type="button" sx={{ p: 1 }}>
+          <SearchOutlined />
+        </IconButton>
+      </Box>
+      {/** other icons */}
+      <Box></Box>
+    </Box>
+  );
 };
 
 export default Topbar;
